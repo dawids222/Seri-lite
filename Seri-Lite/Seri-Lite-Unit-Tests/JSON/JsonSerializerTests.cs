4ee77b62cd266b4783fc8f9b0a2e30e8719fd7e1
Dawid Grajewski <dawid.grajewski@inwedo.com>
2021-07-27 12:46:47 +0200

    Added new test cases

--- Seri-Lite/Seri-Lite-Unit-Tests/JSON/JsonSerializerTests.cs
+++ Seri-Lite/Seri-Lite-Unit-Tests/JSON/JsonSerializerTests.cs
@@ -50,42 +50,25 @@ namespace Seri_Lite_Unit_Tests.JSON
         {
             public IEnumerator GetEnumerator()
             {
-                yield return (object)new { };
-                yield return (object)1;
-                yield return (object)true;
-                yield return (object)"Test";
-                yield return (object)new { Name = "Test" };
-                yield return (object)new { Person = new { Name = "Test1", Surname = "Test2" } };
-                yield return (object)new { Names = new string[] { "Test1", "Test2" } };
-                yield return (object)new string[] { "Test1", "Test2" };
-                yield return (object)new
-                {
-                    Person = new
-                    {
-                        Name = "Test1",
-                        Age = 18,
-                        Height = 180.5,
-                        Married = false,
-                        Address = new
-                        {
-                            City = "Test2",
-                            Street = "Test3"
-                        },
-                        Pets = new object[]
-                        {
-                            new
-                            {
-                                Name="Test4",
-                                Species="Test5"
-                            },
-                            new
-                            {
-                                Name="Test6",
-                                Species="Test7"
-                            },
-                        },
-                    },
-                };
+                yield return new { };
+                yield return Array.Empty<object>();
+                yield return 1;
+                yield return 1.5;
+                yield return true;
+                yield return "Test";
+                yield return new { Name = "Test" };
+                yield return new { Person = new { Name = "Test1", Surname = "Test2" } };
+                yield return new { Names = new string[] { "Test1", "Test2" } };
+                yield return new int[] { 1, 2, 2 };
+                yield return new { Array = new int[] { 1, 2, 2 } };
+                yield return new bool[] { false, true, true };
+                yield return new { Array = new bool[] { false, true, true } };
+                yield return new string[] { "Test1", "Test2", "Test2" };
+                yield return new { Array = new string[] { "Test1", "Test2", "Test2" } };
+                yield return new string[][] { new string[] { "Test1", "Test2" }, new string[] { "Test3", "Test4" } };
+                yield return new object[] { "Test", 1, 1.55, true };
+                yield return new object[] { new { Name = "Test1", Surname = "Test2" }, new { Name = "Test3", Surname = "Test4" } };
+                yield return new { Person = new { Name = "Test1", Age = 18, Height = 180.5, Married = false, Address = new { City = "Test2", Street = "Test3" }, Pets = new object[] { new { Name = "Test4", Species = "Test5" }, new { Name = "Test6", Species = "Test7" }, }, }, };
             }
         }
     }
