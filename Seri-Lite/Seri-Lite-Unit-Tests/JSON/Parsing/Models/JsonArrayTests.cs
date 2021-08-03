4bf151057e88eebb612c86765da94f0bf0a48d1d
dgrajewski <dawids115@gmail.com>
2021-08-03 23:00:48 +0200

    WIP JsonArray implementation

---
+++ Seri-Lite/Seri-Lite-Unit-Tests/JSON/Parsing/Models/JsonArrayTests.cs
@@ -0,0 +1,155 @@
+﻿using NUnit.Framework;
+using Seri_Lite.JSON.Parsing.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Seri_Lite_Unit_Tests.JSON.Parsing.Models
+{
+    [TestFixture]
+    public class JsonArrayTests
+    {
+        private JsonArray _jsonArray;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _jsonArray = new JsonArray();
+        }
+
+        [Test]
+        public void IsObject_ReturnsFalse()
+        {
+            Assert.IsFalse(_jsonArray.IsObject);
+        }
+
+        [Test]
+        public void IsArray_ReturnsTrue()
+        {
+            Assert.IsTrue(_jsonArray.IsArray);
+        }
+
+        [Test]
+        public void IsPrimitive_ReturnsFalse()
+        {
+            Assert.IsFalse(_jsonArray.IsPrimitive);
+        }
+
+        [Test]
+        public void AsObject_ReturnsNull()
+        {
+            var jsonObject = _jsonArray.AsObject();
+
+            Assert.IsNull(jsonObject);
+        }
+
+        [Test]
+        public void AsArray_ReturnsJsonArray()
+        {
+            var jsonArray = _jsonArray.AsArray();
+
+            Assert.IsNotNull(jsonArray);
+        }
+
+        [Test]
+        public void AsPrimitive_ReturnsNull()
+        {
+            var jsonPrimitive = _jsonArray.AsPrimitive();
+
+            Assert.IsNull(jsonPrimitive);
+        }
+
+        [Test]
+        public void Parent_HasNoParent_ReturnsNull()
+        {
+            Assert.IsNull(_jsonArray.Parent);
+        }
+
+        [Test]
+        public void Parent_HasParent_ReturnsParent()
+        {
+            var parent = new JsonArray();
+            var child = new JsonArray(parent);
+
+            Assert.AreEqual(parent, child.Parent);
+        }
+
+        [Test]
+        public void Root_HasNoParent_ReturnsItself()
+        {
+            Assert.AreEqual(_jsonArray, _jsonArray.Root);
+        }
+
+        [Test]
+        public void Root_HasParent_ReturnsRoot()
+        {
+            var root = new JsonArray();
+            var parent = new JsonArray(root);
+            var child = new JsonArray(parent);
+
+            Assert.AreEqual(root, child.Root);
+        }
+
+        [Test]
+        public void Count_ReturnsNumberOfContainedTokens()
+        {
+            var primitives = new JsonPrimitive[]
+            {
+                new JsonPrimitive(),
+                new JsonPrimitive(),
+            };
+
+            _jsonArray = new JsonArray(primitives);
+
+            Assert.AreEqual(primitives.Length, _jsonArray.Count);
+        }
+
+        [TestCaseSource(typeof(HasOnlyTestBundleSource))]
+        public void HasOnly_ReturnsValueDependentOnTheSituation(HasOnlyTestBundle bundle)
+        {
+            _jsonArray = new JsonArray(bundle.Tokens);
+
+            Assert.AreEqual(bundle.HasOnly.Objects, _jsonArray.HasOnlyObjects);
+            Assert.AreEqual(bundle.HasOnly.Arrays, _jsonArray.HasOnlyArrays);
+            Assert.AreEqual(bundle.HasOnly.Primitives, _jsonArray.HasOnlyPrimitives);
+            Assert.AreEqual(bundle.HasOnly.Mixed, _jsonArray.HasMixedTokens);
+        }
+
+        class HasOnlyTestBundleSource : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new HasOnlyTestBundle(
+                    new JsonToken[] { new JsonObject() },
+                    new HasOnlyTest(Objects: true));
+                yield return new HasOnlyTestBundle(
+                    new JsonToken[] { new JsonArray() },
+                    new HasOnlyTest(Arrays: true));
+                yield return new HasOnlyTestBundle(
+                    new JsonToken[] { new JsonPrimitive() },
+                    new HasOnlyTest(Primitives: true));
+                yield return new HasOnlyTestBundle(
+                    Array.Empty<JsonToken>(),
+                    new HasOnlyTest());
+                yield return new HasOnlyTestBundle(
+                    new JsonToken[]
+                    {
+                        new JsonObject(),
+                        new JsonArray(),
+                        new JsonPrimitive(),
+                    },
+                    new HasOnlyTest(Mixed: true));
+            }
+        }
+
+        public record HasOnlyTestBundle(
+            IEnumerable<JsonToken> Tokens,
+            HasOnlyTest HasOnly);
+
+        public record HasOnlyTest(
+            bool Objects = false,
+            bool Arrays = false,
+            bool Primitives = false,
+            bool Mixed = false);
+    }
+}
--- Seri-Lite/Seri-Lite/JSON/Parsing/Models/JsonArray.cs
+++ Seri-Lite/Seri-Lite/JSON/Parsing/Models/JsonArray.cs
@@ -1,6 +1,40 @@
-﻿namespace Seri_Lite.JSON.Parsing.Models
+﻿using Seri_Lite.JSON.Parsing.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Seri_Lite.JSON.Parsing.Models
 {
     public class JsonArray : JsonToken
     {
+        public override JsonTokenType TokenType => JsonTokenType.ARRAY;
+
+        private readonly List<JsonToken> _tokens = new();
+
+        public JsonArray() : this(null, Array.Empty<JsonToken>()) { }
+        public JsonArray(JsonToken parent) : this(parent, Array.Empty<JsonToken>()) { }
+        public JsonArray(IEnumerable<JsonToken> tokens) : this(null, tokens) { }
+        public JsonArray(JsonToken parent, IEnumerable<JsonToken> tokens) : base(parent)
+        {
+            _tokens.AddRange(tokens);
+        }
+
+        public int Count => _tokens.Count;
+        public bool IsEmpty => Count == 0;
+
+        public bool HasOnlyObjects => !IsEmpty && GetTokens().All(t => t.IsObject);
+        public bool HasOnlyArrays => !IsEmpty && GetTokens().All(t => t.IsArray);
+        public bool HasOnlyPrimitives => !IsEmpty && GetTokens().All(t => t.IsPrimitive);
+        public bool HasMixedTokens => !IsEmpty && GetTokens().Select(t => t.TokenType).Distinct().Count() > 1;
+
+        public IEnumerable<JsonToken> GetTokens() => _tokens;
+        public IEnumerable<JsonObject> GetObjects() => _tokens.Cast<JsonObject>();
+        public IEnumerable<JsonArray> GetArrays() => _tokens.Cast<JsonArray>();
+        public IEnumerable<JsonPrimitive> GetPrimitives() => _tokens.Cast<JsonPrimitive>();
+
+        public JsonToken GetToken(int index) => GetTokens().ElementAt(index);
+        public JsonObject GetObject(int index) => GetObjects().ElementAt(index);
+        public JsonArray GetArray(int index) => GetArrays().ElementAt(index);
+        public JsonPrimitive GetPrimitive(int index) => GetPrimitives().ElementAt(index);
     }
 }
